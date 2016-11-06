65b84b7de839f66ddd37fb2e57451d9033651cbe
Stefano Gariazzo <steog88@gmail.com>
2016-11-06 20:45:55 +0100

    import localPDF, changed initial display message

--- pybiblio/cli.py
+++ pybiblio/cli.py
@@ -7,15 +7,15 @@ try:
 	import pybiblio.webimport.webInterf as webInt
 	from pybiblio.config import pbConfig
 	from pybiblio.database import pBDB
+	from pybiblio.pdf import pBPDF
 except ImportError:
 	print("[CLI] Could not find pybiblio and its contents: configure your PYTHONPATH!")
 	print traceback.format_exc()
 
 def cli():
-	print("[CLI] Activating CommandLineInterface")
-	print("Write a command and press Enter (leave empty and press Enter to exit):")	
 	vars = globals().copy()
 	vars.update(locals())
 	shell = code.InteractiveConsole(vars)
-	shell.interact()
+	shell.interact("""[CLI] Activating CommandLineInterface
+Write a command and press Enter ('Ctrl+D' or 'exit()' to exit).""")
 	print("[CLI] CommandLineInterface closed.")
