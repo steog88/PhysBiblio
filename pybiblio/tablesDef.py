2bcc21040b9b51a9dbeab069f7a5c494b3814f45
Stefano Gariazzo <steog88@gmail.com>
2017-01-02 23:41:45 +0100

    fields descriptions, more order

--- pybiblio/tablesDef.py
+++ pybiblio/tablesDef.py
@@ -1,58 +1,99 @@
 """definitions of the tables for the database"""
 tableFields = {}
-fieldsDescriptions = {}
 tableFields["entries"] = [
-	["bibkey", "text", "primary key not null"],
+	["bibkey", 	"text", "primary key not null"],
 	["inspire", "text", ""],
-	["arxiv", "text", ""],
-	["ads", "text", ""],
+	["arxiv", 	"text", ""],
+	["ads", 	"text", ""],
 	["scholar", "text", ""],
-	["doi", "text", ""],
-	["isbn", "text", ""],
-	["year", "int", ""],
-	["link", "text", ""],
-	["comments", "text", ""],
-	["old_keys", "text", ""],
-	["crossref", "text", ""],
-	["bibtex", "text", "not null"],
-	["firstdate", "text", "not null"],
-	["pubdate", "text", ""],
+	["doi", 	"text", ""],
+	["isbn", 	"text", ""],
+	["year", 	"int", ""],
+	["link", 	"text", ""],
+	["comments", 	"text", ""],
+	["old_keys", 	"text", ""],
+	["crossref", 	"text", ""],
+	["bibtex", 		"text", "not null"],
+	["firstdate", 	"text", "not null"],
+	["pubdate", 	"text", ""],
 	["exp_paper",	"int", "default 0"],
 	["lecture",		"int", "default 0"],
 	["phd_thesis",	"int", "default 0"],
 	["review",		"int", "default 0"],
 	["proceeding",	"int", "default 0"],
 	["book",		"int", "default 0"],
-	["marks", "text", ""]];
+	["marks", 		"text", ""]];
 tableFields["categories"] = [
-	["idCat", "integer", "primary key"],
-	["name", "text", "not null"],
+	["idCat", 		"integer", "primary key"],
+	["name", 		"text", "not null"],
 	["description", "text", "not null"],
-	["parentCat", "int", "default 0"],
-	["comments", "text", "default ''"],
-	["ord", "int", "default 0"]];
+	["parentCat", 	"int", "default 0"],
+	["comments", 	"text", "default ''"],
+	["ord", 		"int", "default 0"]];
 tableFields["experiments"] = [
-	["idExp", "integer", "primary key"],
-	["name", "text", "not null"],
+	["idExp", 	 "integer", "primary key"],
+	["name", 	 "text", "not null"],
 	["comments", "text", "not null"],
 	["homepage", "text", ""],
-	["inspire", "text", ""]];
+	["inspire",  "text", ""]];
 tableFields["expCats"] = [
 	["idExC", "integer", "primary key"],
 	["idExp", "int", "not null"],
 	["idCat", "int", "not null"]];
 tableFields["entryCats"] = [
-	["idEnC", "integer", "primary key"],
+	["idEnC",  "integer", "primary key"],
 	["bibkey", "text", "not null"],
-	["idCat", "int", "not null"]];
+	["idCat",  "int", "not null"]];
 tableFields["entryExps"] = [
 	["idEnEx", "integer", "primary key"],
 	["bibkey", "text", "not null"],
-	["idExp", "int", "not null"]];
-
+	["idExp",  "int", "not null"]];
+fieldsDescriptions = {}
+fieldsDescriptions["entries"] = {
+	"bibkey":	"Unique bibtex key that identifies the bibliographic element",
+	"inspire":	"Inspire ID of the record (if any)",
+	"arxiv":	"arXiv ID of the record (if any)",
+	"ads":		"NASA ADS ID of the record (if any)",
+	"scholar":	"Google Scholar ID of the record (if any)",
+	"doi":		"DOI of the record (if any)",
+	"isbn":		"ISBN code of the record (if any)",
+	"year":		"Year of publication",
+	"link":		"Web link to article or additional material",
+	"comments":	"Comments",
+	"old_keys":	"Previous bibtex keys of the record",
+	"crossref":	"Bibtex crossref reference",
+	"bibtex":	"Bibtex entry",
+	"firstdate":"Date of first appearance",
+	"pubdate":	"Date of publication",
+	"exp_paper": "(T/F) The entry is a collaboration paper of some experiment",
+	"lecture":	 "(T/F) The entry is a lecture",
+	"phd_thesis":"(T/F) The entry is a PhD thesis",
+	"review":	 "(T/F) The entry is a review",
+	"proceeding":"(T/F) The entry is a proceeding",
+	"book":		 "(T/F) The entry is a book",
+	"marks":	"Assign a mark to the record (not yet implemented)"}
+fieldsDescriptions["categories"] = {
+	"idCat": 		"Unique ID that identifies the category",
+	"name": 		"Name of the category",
+	"description": 	"Description of the category",
+	"parentCat": 	"Parent category",
+	"comments": 	"Comments",
+	"ord": 			"Ordering when plotting (not yet implemented)"}
 fieldsDescriptions["experiments"] = {
-	"idExp": "unique ID that identifies the experiment",
-	"name": "Name of the experiment",
+	"idExp": 	"Unique ID that identifies the experiment",
+	"name": 	"Name of the experiment",
 	"comments": "Description or comments",
-	"homepage": "web link to the experiment homepage",
-	"inspire": "the Inspire ID of the experiment record"}
+	"homepage": "Web link to the experiment homepage",
+	"inspire": 	"Inspire ID of the experiment record"}
+fieldsDescriptions["expCats"] = {
+	"idExC": "Unique identifier",
+	"idExp": "Corresponding experiment ID",
+	"idCat": "Corresponding category ID"}
+fieldsDescriptions["entryCats"] = {
+	"idEnC":  "Unique identifier",
+	"bibkey": "Corresponding bibtex key",
+	"idCat":  "Corresponding category ID"}
+fieldsDescriptions["entryExps"] = {
+	"idEnEx": "Unique identifier",
+	"bibkey": "Corresponding bibtex key",
+	"idExp":  "Corresponding experiment ID"}
