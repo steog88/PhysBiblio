986fe2d42ed9fd391ab5958173fe5fabb3edcf3c
Stefano Gariazzo <steog88@gmail.com>
2018-04-06 11:43:13 +0200

    command line triggered functions of main script (help, cli, tex)

--- physbiblio.py
+++ physbiblio.py
@@ -18,8 +18,46 @@ except ImportError:
 except Exception:
 	print(traceback.format_exc())
 
+def TorF(s):
+	"""Get a True or False value from a command line option"""
+	if s.lower() == "t" or s == 1 or s.lower() == "true":
+		return True
+	elif s.lower() == "f" or s == 0 or s.lower() == "false":
+		return True
+	else:
+		return None
+
 # GUI application
 if __name__=='__main__':
+	command = None
+	if len(sys.argv) > 1:
+		command = sys.argv[1].lower()
+	if command == "help":
+		print("\nAvailable command line options:")
+		print("* help: print this text;")
+		print("* cli: open a command line where to directly run PhysBiblio functions;")
+		print("* tex: read .tex file(s) and create a *.bib file with the cited bibtexs.")
+		print("")
+		exit()
+	elif command == "cli":
+		from physbiblio.cli import cli as physBiblioCLI
+		physBiblioCLI()
+		exit()
+	elif command == "tex":
+		from physbiblio.export import pBExport
+		if len(sys.argv) < 4:
+			print("\nWrong usage of command line command 'tex': mandatory arguments are missing. Please use:")
+			print("`physbiblio.py tex '/folder/where/tex/files/are/' 'name_of_output.bib' [overwrite [autosave]]`")
+			print("(the optional arguments overwrite and autosave may be 1,T,t,True,true for True or 0,F,f,False,false for False. Default: overwrite = False, autosave = True)\n")
+		texFiles = sys.argv[2]
+		bibFile = sys.argv[3]
+		overwrite = TorF(sys.argv[4]) if len(sys.argv) > 4 else False
+		autosave = TorF(sys.argv[5]) if len(sys.argv) > 5 else True
+		if overwrite is None or autosave is None:
+			print("\nInvalid value for overwrite or autosave. You may use 1,T,t,True,true for True or 0,F,f,False,false for False.\n")
+		pBExport.exportForTexFile(texFiles, bibFile, overwrite = overwrite, autosave = autosave)
+		exit()
+
 	try:
 		app = QApplication(sys.argv)
 		mainWin = MainWindow()
