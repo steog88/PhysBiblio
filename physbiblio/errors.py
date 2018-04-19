8f19b618774c5188d485fcd4a149a6ee3c508cf9
Stefano Gariazzo <steog88@gmail.com>
2018-04-19 22:33:44 +0200

    removed toOut handler, added functions to add temporary handler, different levels for each handler, pBLogger as shortcut (will be used everywhere)

--- physbiblio/errors.py
+++ physbiblio/errors.py
@@ -17,6 +17,7 @@ class pBErrorManagerClass():
 		"""
 		Constructor for PBErrorManager.
 		"""
+		self.tempsh = None
 		if pbConfig.params["loggingLevel"] == 0:
 			self.loglevel = logging.ERROR
 		elif pbConfig.params["loggingLevel"] == 1:
@@ -28,30 +29,43 @@ class pBErrorManagerClass():
 		#the main logger, will save to stdout and log file
 		self.logger = logging.getLogger("physbibliolog")
 		self.logger.propagate = False
-		self.logger.setLevel(self.loglevel)
+		self.logger.setLevel(min(logging.INFO, self.loglevel))
 
 		fh = logging.FileHandler(pbConfig.params["logFileName"])
+		fh.setLevel(self.loglevel)
 		formatter = logging.Formatter('%(asctime)s %(levelname)10s : [%(module)s.%(funcName)s] %(message)s')
 		fh.setFormatter(formatter)
 		self.logger.addHandler(fh)
 
 		sh = logging.StreamHandler()
+		sh.setLevel(logging.INFO)
 		formatter = logging.Formatter('[%(module)s.%(funcName)s] %(message)s')
 		sh.setFormatter(formatter)
 		self.logger.addHandler(sh)
 
-		#other logger, will be used instead of print and redirected
-		self.toOut = logging.getLogger("physbiblioout")
-		self.toOut.propagate = False
-		self.toOut.setLevel(logging.INFO)
-		sh = logging.StreamHandler()
-		formatter = logging.Formatter('%(message)s')
-		sh.setFormatter(formatter)
-		self.toOut.addHandler(sh)
-
-	def log(self, message, trcbk = None, priority = 0):
+	def tempHandler(self, stream = sys.stdout, level = logging.INFO, format = '[%(module)s.%(funcName)s] %(message)s'):
 		"""
-		Uses a logger for printing and storing the messages in the log file.
+		Set a temporary StreamHandler for the logger, given the stream, with default level logging.INFO.
+		Useful when redirecting stdout
+
+		Parameters:
+			stream: the stream to be used (default: sys.stdout)
+			level: the level to be used (default: logging.INFO)
+			format: the format, using the logging syntax (default: '[%(module)s.%(funcName)s] %(message)s')
+		"""
+		self.tempsh = logging.StreamHandler(stream)
+		self.tempsh.setLevel(level)
+		formatter = logging.Formatter(format)
+		self.tempsh.setFormatter(formatter)
+		self.logger.addHandler(self.tempsh)
+
+	def rmTempHandler(self):
+		self.logger.removeHandler(self.tempsh)
+		self.tempsh = None
+
+	def __call__(self, message, trcbk = None, priority = 0):
+		"""
+		Directly calls the logger for printing and storing the messages in the log file, for backwards compatibility only. Will be removed.
 
 		Parameters:
 			message: a text containing a description of the error
@@ -62,20 +76,5 @@ class pBErrorManagerClass():
 			message += "\n" + trcbk.format_exc()
 		self.logger.log((2+priority)*10, message)
 
-	def out(self, message):
-		"""
-		Uses a logger for printing the messages.
-
-		Parameters:
-			message: a text containing a description of the error
-		"""
-		message += "\n"
-		self.toOut.info(message)
-
-	def __call__(self, *args, **kwargs):
-		"""
-		Redirect to self.log for backwards compatibility
-		"""
-		self.log(*args, **kwargs)
-
 pBErrorManager = pBErrorManagerClass()
+pBLogger = pBErrorManager.logger
