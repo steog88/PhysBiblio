df30df8fc2bd05ca724610c8aa9e6e7b129f1e6d
Stefano Gariazzo <steog88@gmail.com>
2018-04-21 11:38:48 +0200

    reordered things, fixed behavior when exception in main; added pyside as dependency for python2 for auto install

--- physbiblio.py
+++ physbiblio.py
@@ -27,9 +27,24 @@ def TorF(s):
 	else:
 		return None
 
+def call_clean(arguments):
+	startFrom = arguments[0] if len(arguments) > 0 else 0
+	pBDB.bibs.cleanBibtexs(startFrom = startFrom)
+	pBDB.commit()
+	exit()
+
 def call_cli():
 	from physbiblio.cli import cli as physBiblioCLI
 	physBiblioCLI()
+	exit()
+
+def call_export(arguments):
+	from physbiblio.export import pBExport
+	if len(arguments) < 1:
+		print("\nWrong usage of command line command 'export': mandatory argument 'fname' is missing.\n")
+		exit()
+	fname = arguments[0]
+	pBExport.exportAll(fname)
 	exit()
 
 def call_tex(arguments):
@@ -48,25 +63,10 @@ def call_tex(arguments):
 	pBExport.exportForTexFile(texFiles, bibFile, overwrite = overwrite, autosave = autosave)
 	exit()
 
-def call_export(arguments):
-	from physbiblio.export import pBExport
-	if len(arguments) < 1:
-		print("\nWrong usage of command line command 'export': mandatory argument 'fname' is missing.\n")
-		exit()
-	fname = arguments[0]
-	pBExport.exportAll(fname)
-	exit()
-
 def call_update(arguments):
 	startFrom = int(arguments[0]) if len(arguments) > 0 else 0
 	force = TorF(arguments[1]) if len(arguments) > 1 else False
 	pBDB.bibs.searchOAIUpdates(startFrom = startFrom, force = force)
-	pBDB.commit()
-	exit()
-
-def call_clean(arguments):
-	startFrom = arguments[0] if len(arguments) > 0 else 0
-	pBDB.bibs.cleanBibtexs(startFrom = startFrom)
 	pBDB.commit()
 	exit()
 
@@ -75,7 +75,6 @@ def call_oaiDates(date1, date2):
 	pBDB.commit()
 	exit()
 
-# GUI application
 if __name__=='__main__':
 	try:
 		command = None
@@ -85,25 +84,30 @@ if __name__=='__main__':
 		if command == "help":
 			print("\nAvailable command line options:")
 			print("* help: print this text;")
+			print("* clean: parse and clean all the bibtex entries in the database;")
 			print("* cli: open a command line where to directly run PhysBiblio functions;")
 			print("* daily: retrieve records changed in the day from InspireOAI and update entries;")
 			print("* export: export all the DB entries in a *.bib file;")
 			print("* tex: read .tex file(s) and create a *.bib file with the cited bibtexs;")
-			print("* update: use INSPIRE to update the information in the database.")
+			print("* update: use INSPIRE to update the information in the database;")
+			print("* weekly: retrieve records changed in the week from InspireOAI and update entries.")
 			print("")
 			exit()
-		elif command == "cli":
-			call_cli()
-		elif command == "tex":
-			call_tex(sys.argv[2:])
-		elif command == "export":
-			call_export(sys.argv[2:])
-		elif command == "update":
-			call_update(sys.argv[2:])
 		elif command == "clean":
 			call_clean(sys.argv[2:])
+		elif command == "cli":
+			call_cli()
 		elif command == "daily":
 			date1 = (datetime.date.today() - datetime.timedelta(1)).strftime("%Y-%m-%d")
+			call_oaiDates(date1, dateLast)
+		elif command == "export":
+			call_export(sys.argv[2:])
+		elif command == "tex":
+			call_tex(sys.argv[2:])
+		elif command == "update":
+			call_update(sys.argv[2:])
+		elif command == "weekly":
+			date1 = (datetime.date.today() - datetime.timedelta(7)).strftime("%Y-%m-%d")
 			call_oaiDates(date1, dateLast)
 
 		app = QApplication(sys.argv)
@@ -112,9 +116,9 @@ if __name__=='__main__':
 		mainWin.raise_()
 		sys.exit(app.exec_())
 	except NameError:
-		pBLogger.critical("NameError:",sys.exc_info()[1])
+		pBLogger.critical("NameError:", exc_info = True)
 	except SystemExit:
 		pBDB.closeDB()
 		pBLogger.info("Closing main window...")
-	except Exception:
-		pBLogger.critical(traceback.format_exc())
+	except Exception as e:
+		pBLogger.critical(str(e), exc_info = True)
