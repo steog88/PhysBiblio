ea734193b8f93524773ea1020f78beb680181cf2
Stefano Gariazzo <steog88@gmail.com>
2018-05-21 19:23:32 +0200

    version and changelog

--- physbiblio/__init__.py
+++ physbiblio/__init__.py
@@ -6,8 +6,7 @@ This file is part of the physbiblio package.
 __author__ = 'Stefano Gariazzo'
 __email__ = "stefano.gariazzo@gmail.com"
 
-__version__ = "0.4.0"
-__version_date__ = "20/05/2018"
+__version__ = "0.4.1"
+__version_date__ = "21/05/2018"
 
 __all__ = ["bibtexwriter", "cli", "config", "databaseCore", "database", "errors", "export", "inspireStats", "parse_accents", "pdf", "tablesDef", "tests", "view", "gui", "webimport"]
-
