356565689b4a456aaee785dd5a73d06283503330
Stefano Gariazzo <steog88@gmail.com>
2018-05-24 00:16:41 +0200

    preparing arxiv daily rss reader; arxiv categories

--- physbiblio/webimport/arxiv.py
+++ physbiblio/webimport/arxiv.py
@@ -30,8 +30,31 @@ class webSearch(webInterf):
 		self.name = "arXiv"
 		self.description = "arXiv fetcher"
 		self.url = "http://export.arxiv.org/api/query"
+		self.urlRss = "http://export.arxiv.org/rss/"
 		self.urlArgs = {
 			"start":"0"}
+		self.categories = {
+			"astro-ph": ["", "CO", "EP", "GA", "HE", "IM", "SR"],
+			"cond-mat": ["dis-nn", "mes-hall", "mtrl-sci", "other", "quant-gas", "soft", "stat-mech", "str-el", "supr-con"],
+			"cs": ["AI", "AR", "CC", "CE", "CG", "CL", "CR", "CV", "CY", "DB", "DC", "DL", "DM", "DS", "ET", "FL", "GL", "GR", "GT", "HC", "IR", "IT", "LG", "LO", "MA", "MM", "MS", "NA", "NE", "NI", "OH", "OS", "PF", "PL", "RO", "SC", "SD", "SE", "SI", "SY"],
+			"econ": ["EM"],
+			"eess": ["AS", "IV", "SP"],
+			"gr-qc": [""],
+			"hep-ex": [""],
+			"hep-lat": [""],
+			"hep-ph": [""],
+			"hep-th": [""],
+			"math": ["AC", "AG", "AP", "AT", "CA", "CO", "CT", "CV", "DG", "DS", "FA", "GM", "GN", "GR", "GT", "HO", "IT", "KT", "LO", "MG", "MP", "NA", "NT", "OA", "OC", "PR", "QA", "RA", "RT", "SG", "SP", "ST"],
+			"math-ph": [""],
+			"nlin": ["AO", "CD", "CG", "PS", "SI"],
+			"nucl-ex": [""],
+			"nucl-th": [""],
+			"physics": ["acc-ph", "ao-ph", "app-ph", "atm-clus", "atom-ph", "bio-ph", "chem-ph", "class-ph", "comp-ph", "data-an", "ed-ph", "flu-dyn", "gen-ph", "geo-ph", "hist-ph", "ins-det", "med-ph", "optics", "plasm-ph", "pop-ph", "soc-ph", "space-ph"],
+			"q-bio": ["BM", "CB", "GN", "MN", "NC", "OT", "PE", "QM", "SC", "TO"],
+			"q-fin": ["CP", "EC", "GN", "MF", "PM", "PR", "RM", "ST", "TR"],
+			"quant-ph": [""],
+			"stat": ["AP", "CO", "ME", "ML", "OT", "TH"]
+			}
 		
 	def retrieveUrlFirst(self, string, searchType = "all", **kwargs):
 		"""
@@ -142,3 +165,52 @@ class webSearch(webInterf):
 				return "",  {}
 			else:
 				return ""
+
+	def arxivDaily(self, category):
+		"""
+		Read daily RSS feed for a given category
+		"""
+		if "." in category:
+			main, sub = category.split(".")
+		else:
+			main = category
+			sub = ""
+		url = self.urlRss
+		if main not in self.categories.keys():
+			pBLogger.warning("Main category not found: %s"%main)
+			return False
+		else:
+			url += main
+		if sub != "" and sub not in self.categories[main]:
+			pBLogger.warning("Sub category not found: %s"%sub)
+			return False
+		elif sub != "" and sub in self.categories[main]:
+			url += "." + sub
+		pBLogger.info(url)
+		text = self.textFromUrl(url)
+		if text is None:
+			pBLogger.warning("Url is empty!")
+			return False
+		author = re.compile('>([^<]*)</a>')
+		mainCat = re.compile('\[([\-\.a-zA-Z]*)\]')
+		versionFinder = re.compile('arXiv:([0-9\.v]*) ')
+		additionalInfo = re.compile(' \(arXiv:([0-9\.v]*) \[([\-\.a-zA-Z]*)\]([ A-Z]*)\)')
+		try:
+			data = feedparser.parse(parse_accents_str(text.decode("utf-8")))
+			entries = []
+			for element in data.entries:
+				tmp = {}
+				tmp["eprint"] = element["id"].split("/")[-1]
+				tmp["abstract"] = element["summary"].replace("\n", " ").replace("<p>", "").replace("</p>", "")
+				tmp["authors"] = [ m.group().replace("</a>", "").replace(">", "") for m in author.finditer(element["authors"][0]["name"]) if m != "" ]
+				tmp["cross"] = "CROSS LISTED" in element["title"]
+				tmp["replacement"] = "UPDATED" in element["title"]
+				tmp["mainCat"] = [m.group(2) for m in additionalInfo.finditer(element["title"]) if m != ""][0]
+				tmp["version"] = [m.group(1) for m in additionalInfo.finditer(element["title"]) if m != ""][0]
+				parenthesis = [m.group() for m in additionalInfo.finditer(element["title"]) if m != ""][0]
+				tmp["title"] = element["title"].replace(parenthesis, "")
+				entries.append(tmp)
+			return entries
+		except Exception:
+			pBLogger.error("Cannot parse arxiv RSS feed:\n%s"%text, exc_info = True)
+			return False
