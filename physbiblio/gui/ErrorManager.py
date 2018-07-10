96cae52e4cfa454e363833408f90c7bc67790abb
Stefano Gariazzo <steog88@gmail.com>
2018-07-10 23:47:02 +0200

    docstrings and minor changes

--- physbiblio/gui/ErrorManager.py
+++ physbiblio/gui/ErrorManager.py
@@ -14,15 +14,39 @@ except ImportError:
 	print("Could not find physbiblio and its contents: configure your PYTHONPATH!")
 
 class ErrorStream(StringIO):
+	"""
+	Define a stream based on StringIO, which opens a `QMessageBox` when `write` is called
+	"""
 	def __init__(self, *args, **kwargs):
+		"""
+		The constructor, which passes the `*args, **kwargs` method to StringIO.__init__.
+		"""
 		StringIO.__init__(self, *args, **kwargs)
 		self.priority = 1
 		self.lastMBox = None
 
 	def setPriority(self, prio):
+		"""
+		Set the priority level to be adopted when opening the `QMessageBox`.
+
+		Parameter:
+			prio: the priority level. 0, 1, 2+ correspond to `Information`, `Warning`, `Error`
+		"""
 		self.priority = prio
 
 	def write(self, text, testing = False):
+		"""
+		Override the `write` method of `StringIO` to show a `QMessageBox`, with icon according to the current priority.
+		The priority is set to 1 after execution.
+
+		Parameters:
+			text: the text to display. "\n" is replaced with "<br>".
+			testing (boolean, optional, default False): when doing tests, interrupt the execution before `exec_` is run and return the `QMessageBox` object.
+
+		Output:
+			if text is empty or testing is False, return None
+			if testing is True, return the `QMessageBox` object
+		"""
 		if text.strip() == "":
 			return
 		text = text.replace('\n', '<br>')
@@ -41,21 +65,31 @@ class ErrorStream(StringIO):
 		if testing:
 			return self.lastMBox
 		self.lastMBox.exec_()
+		return
 
 class pBErrorManagerClassGui(pBErrorManagerClass):
 	"""
-	Extends the error manager class to show gui messages
+	Extends the error manager class to show gui messages through ErrorStream
 	"""
 	def __init__(self):
 		"""
-		Init the class, storing the name of the external web application
-		and the base strings to build some links
+		Init the class, using pBErrorManagerClass.__init__ and the gui logger name,
+		then add a new handler which uses ErrorStream
 		"""
 		pBErrorManagerClass.__init__(self, "physbiblioguilog")
 		self.guiStream = ErrorStream()
 		self.tempHandler(self.guiStream, level = logging.DEBUG, format = '%(message)s')
 
 	def loggerPriority(self, prio):
+		"""
+		Define the priority level that must be used by the `ErrorStream` class in the first call of its `write` method.
+
+		Parameter:
+			prio: the priority level. 0, 1, 2+ correspond to Information, Warning, Error
+
+		Output:
+			the logger [so it can be used as `pBGUIErrorManager.loggerPriority(prio).info(message)`]
+		"""
 		self.guiStream.priority = prio
 		return self.logger
 
