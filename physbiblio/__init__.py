72265acdb8103a47541549952a8fc6a7954341b3
Stefano Gariazzo <steog88@gmail.com>
2018-10-09 16:04:14 +0200

    bug when default profile is not marked, new version released

--- physbiblio/__init__.py
+++ physbiblio/__init__.py
@@ -6,7 +6,7 @@ This file is part of the physbiblio package.
 __author__ = 'Stefano Gariazzo'
 __email__ = "stefano.gariazzo@gmail.com"
 
-__version__ = "0.6.0"
-__version_date__ = "03/10/2018"
+__version__ = "0.6.1"
+__version_date__ = "09/10/2018"
 
 __all__ = ["bibtexwriter", "cli", "config", "databaseCore", "database", "errors", "export", "inspireStats", "parse_accents", "pdf", "tablesDef", "tests", "view", "gui", "webimport"]
