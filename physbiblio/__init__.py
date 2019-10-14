4013d65bb892d79054024791c5a6db47a6637bfa
Stefano Gariazzo <steog88@gmail.com>
2019-10-14 12:41:08 +0200

    version and changelog update

--- physbiblio/__init__.py
+++ physbiblio/__init__.py
@@ -6,8 +6,8 @@ This file is part of the physbiblio package.
 __author__ = "Stefano Gariazzo"
 __email__ = "stefano.gariazzo@gmail.com"
 
-__version__ = "1.4.0"
-__version_date__ = "08/10/2019"
+__version__ = "1.4.1"
+__version_date__ = "14/10/2019"
 
 __all__ = [
     "bibtexWriter",
@@ -28,9 +28,7 @@ __all__ = [
 ]
 
 __recent_changes__ = """<br>
-* function to show recent version changes from the GUI (shown when opening the program for the first time with a new version);<br>
-* option to make the update notification less invasive (it will be shown in the status bar instead of as a message at startup);<br>
-* solved bug affecting database.Entries.getByKey when simple keys are used;<br>
-* some new tests for package properties and fixes to adsnasa offline tests;<br>
-* several internal changes and test improvements.<br>
+* disable already useless stop button when a thread is finished;<br>
+* attempt to fix "QThread: Destroyed while thread is still running";<br>
+* fixing bugs with progress reporting in GUI for threaded process (and some functions improved meanwhile).<br>
 """
