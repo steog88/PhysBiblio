55b15798e8d0f1920c5f7dce3a895a87d1bc6e1c
Stefano Gariazzo <steog88@gmail.com>
2020-07-21 11:44:50 +0200

    version info in separate file

--- physbiblio/__init__.py
+++ physbiblio/__init__.py
@@ -6,9 +6,6 @@ This file is part of the physbiblio package.
 __author__ = "Stefano Gariazzo"
 __email__ = "stefano.gariazzo@gmail.com"
 
-__version__ = "1.6.1"
-__version_date__ = "20/07/2020"
-
 __all__ = [
     "bibtexWriter",
     "cli",
@@ -28,14 +25,4 @@ __all__ = [
     "webimport",
 ]
 
-__recent_changes__ = """<br>
-<b>1.6.0</b>:<br>
-* changed most of the code to use the new INSPIRE API<br>
-* double click links point to new INSPIRE<br>
-* bug when importing from arxiv daily listing<br>
-* python2 support dropped! The code may still work, but it is not tested.<br>
-* isort for ordering imports<br>
-* pre-commit hooks to check code formatting and stuff<br>
-<br>
-<b>1.6.1</b>: I forgot to update the CHANGELOG!<br>
-"""
+from .version import __recent_changes__, __version__, __version_date__
---
+++ physbiblio/version.py
@@ -0,0 +1,14 @@
+__version__ = "1.6.1"
+__version_date__ = "20/07/2020"
+
+__recent_changes__ = """<br>
+<b>1.6.0</b>:<br>
+* changed most of the code to use the new INSPIRE API<br>
+* double click links point to new INSPIRE<br>
+* bug when importing from arxiv daily listing<br>
+* python2 support dropped! The code may still work, but it is not tested.<br>
+* isort for ordering imports<br>
+* pre-commit hooks to check code formatting and stuff<br>
+<br>
+<b>1.6.1</b>: I forgot to update the CHANGELOG!<br>
+"""
