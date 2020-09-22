17144d2a1d313aceaa6a607923c47a6d71854979
Stefano Gariazzo <steog88@gmail.com>
2020-09-22 16:07:14 +0200

    minor version release

--- physbiblio/version.py
+++ physbiblio/version.py
@@ -1,7 +1,11 @@
-__version__ = "1.6.1"
-__version_date__ = "xxx"
+__version__ = "1.6.2"
+__version_date__ = "22/09/2020"
 
-__recent_changes__ = """<br>* fixes to inspireStats tests<br>
+__recent_changes__ = """<br>* show error message when an import with corrupted bibtex is failed<br>
+* fixes to inspireStats tests<br>
+* fixed bug with selection of bibtexs<br>
 * python2 fix<br>
-* yaml to manage the CHANGELOG and version number, automatically updated through make<br>
+* yaml to manage the CHANGELOG and version number, automatically updated through make before upload<br>
+* black style updates<br>
+* fixing a bug in tests with pyside2 5.15.1<br>
 """
