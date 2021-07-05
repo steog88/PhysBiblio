f0756e734e04a549bb7aa872ca7c91dfb2d4e1aa
Stefano Gariazzo <steog88@gmail.com>
2021-07-05 11:26:51 +0200

    version update

--- physbiblio/version.py
+++ physbiblio/version.py
@@ -1,9 +1,7 @@
-__version__ = "1.6.4"
-__version_date__ = "31/05/2021"
+__version__ = "1.7.0"
+__version_date__ = "05/07/2021"
 
-__recent_changes__ = """<br>* fix errors in MacOS<br>
-* fix problem with multiple '@' in bibtex during import<br>
-* increase time separation between some INSPIRE queries<br>
-* small bug fixes and deprecation errors solved<br>
-* test fixes and updates<br>
+__recent_changes__ = """<br>* replaced calls to old INSPIRE OAI with new API<br>
+* bug fixing<br>
+* some dependencies updated<br>
 """
