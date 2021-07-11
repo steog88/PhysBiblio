34774c7befe00883ea9ffdff11af8b2c741f86a7
Stefano Gariazzo <steog88@gmail.com>
2021-07-11 12:55:09 +0200

    new minor version released

--- physbiblio/version.py
+++ physbiblio/version.py
@@ -1,7 +1,7 @@
-__version__ = "1.7.0"
-__version_date__ = "05/07/2021"
+__version__ = "1.7.1"
+__version_date__ = "11/07/2021"
 
-__recent_changes__ = """<br>* replaced calls to old INSPIRE OAI with new API<br>
-* bug fixing<br>
-* some dependencies updated<br>
+__recent_changes__ = """<br>* database can now store citation counts from inspire<br>
+* improvements to arxiv module<br>
+* small improvements to some error messages<br>
 """
