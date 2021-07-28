9185a548370b3d08747bbfd93f92f86acedc583b
Stefano Gariazzo <steog88@gmail.com>
2021-07-29 00:33:23 +0200

    version updated

--- physbiblio/version.py
+++ physbiblio/version.py
@@ -1,7 +1,6 @@
-__version__ = "1.7.1"
-__version_date__ = "11/07/2021"
+__version__ = "1.8.0"
+__version_date__ = "29/07/2021"
 
-__recent_changes__ = """<br>* database can now store citation counts from inspire<br>
-* improvements to arxiv module<br>
-* small improvements to some error messages<br>
+__recent_changes__ = """<br>* New features / improvements<br>
+* Bug fixes<br>
 """
