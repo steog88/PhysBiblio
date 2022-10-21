5b2799ca53f0d897611d50115daeb5e9a83ecd60
Stefano Gariazzo <steog88@gmail.com>
2022-10-21 16:32:46 +0200

    version update

--- physbiblio/version.py
+++ physbiblio/version.py
@@ -1,5 +1,10 @@
-__version__ = "2.0.2"
-__version_date__ = "2022-08"
+__version__ = "2.0.3"
+__version_date__ = "21/10/2022"
 
-__recent_changes__ = """<br>* total citation counts when opening selection/right-click menu for entries<br>
+__recent_changes__ = """<br>* search entries by "none of the following" categories or experiments<br>
+* possibility to compute average per year and total number of citations in the last N years from the Author stats panel<br>
+* fixed bug when creating menu with empty selection; show number of selected papers in context menu<br>
+* label showing number of currently displayed entries next to filter QLineEdit<br>
+* bug fixes in py3.10 tests<br>
+* PySide6 6.4.0 incompatible with matplotlib up to 3.6.1<br>
 """
