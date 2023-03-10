3974dabe18dd6231d3b17c73b9d0f668ebd81857
Stefano Gariazzo <steog88@gmail.com>
2023-03-10 14:10:04 +0100

    version update

--- physbiblio/version.py
+++ physbiblio/version.py
@@ -1,10 +1,8 @@
-__version__ = "2.0.3"
-__version_date__ = "21/10/2022"
+__version__ = "2.0.4"
+__version_date__ = "10/03/2023"
 
-__recent_changes__ = """<br>* search entries by "none of the following" categories or experiments<br>
-* possibility to compute average per year and total number of citations in the last N years from the Author stats panel<br>
-* fixed bug when creating menu with empty selection; show number of selected papers in context menu<br>
-* label showing number of currently displayed entries next to filter QLineEdit<br>
-* bug fixes in py3.10 tests<br>
-* PySide6 6.4.0 incompatible with matplotlib up to 3.6.1<br>
+__recent_changes__ = """<br>* deprecation warnings fixed in setup.cfg and setup.py<br>
+* code improvements thanks to refurb<br>
+* PySide6 6.4.0 enum system now compatible with matplotlib >3.6.2<br>
+* bug fixes<br>
 """
