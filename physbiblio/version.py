27d598265ad6e2c6d1831a71114bc5b92cdb609d
Stefano Gariazzo <steog88@gmail.com>
2023-08-11 19:03:55 +0200

    version update

--- physbiblio/version.py
+++ physbiblio/version.py
@@ -1,6 +1,17 @@
-__version__ = "2.0.5"
-__version_date__ = "07/06/2023"
+__version__ = "2.1.0"
+__version_date__ = "11/08/2023"
 
-__recent_changes__ = """<br>* fixes for python 3.11<br>
-* ISBN2bibtex scripts dropped (the webservice is no longer working)<br>
+__recent_changes__ = """<br>* search by inspire ID<br>
+* search NULL fields<br>
+* search noUpdate bibs<br>
+* toggle noUpdate from context menu<br>
+* shortcut Ctrl+Enter to quickly accept edit dialogs<br>
+* fixes with triggered signal<br>
+* store query for each tab separately<br>
+* fix in merge function<br>
+* fix when storing last query from tab<br>
+* DB functions checkDuplicates (no quick access to it yet from command line nor GUI) and fetchByField in Entries<br>
+* fix None in various fields when running cleanFields<br>
+* fix messing up when one of the previous searches had more lines than the desider one<br>
+* improved search by key in database (exact match of bibkey or one of the old_keys is required to match)<br>
 """
