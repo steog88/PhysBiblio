e5d6ea4c4d1a2cd271b4df8ed9024069d813bb66
Stefano Gariazzo <steog88@gmail.com>
2023-09-11 19:47:58 +0200

    version update

--- physbiblio/version.py
+++ physbiblio/version.py
@@ -1,7 +1,8 @@
-__version__ = "2.1.1"
-__version_date__ = "24/08/2023"
+__version__ = "2.1.2"
+__version_date__ = "11/09/2023"
 
-__recent_changes__ = """<br>* possibility to call "Type"/"none" search in dictionary to allow noUpdate<br>
-* fixes at "Update/Reload bibtex"<br>
-* automatically update arxiv and doi information when modifying or updating bibtex, or cleaning the entries<br>
+__recent_changes__ = """<br>* function printDuplicates to show the output nicely, command line option to check and print the duplicates<br>
+* exportAll and similar can add citation counts (from the database) to the bibtex<br>
+* citations option for export command line command<br>
+* thread and main window action for checkDuplicates, show duplicates in a table, double click cell to open the duplicates in a new tab<br>
 """
