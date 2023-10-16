41cba40a7b2897f2dfeda3877586ced9613cd9fe
Stefano Gariazzo <steog88@gmail.com>
2023-10-16 12:46:39 +0200

    version published

--- physbiblio/version.py
+++ physbiblio/version.py
@@ -1,8 +1,7 @@
-__version__ = "2.1.2"
-__version_date__ = "11/09/2023"
+__version__ = "2.1.3"
+__version_date__ = "16/09/2023"
 
-__recent_changes__ = """<br>* function printDuplicates to show the output nicely, command line option to check and print the duplicates<br>
-* exportAll and similar can add citation counts (from the database) to the bibtex<br>
-* citations option for export command line command<br>
-* thread and main window action for checkDuplicates, show duplicates in a table, double click cell to open the duplicates in a new tab<br>
+__recent_changes__ = """<br>* exportForTexFile runs checkDuplicates<br>
+* small internal improvements<br>
+* bug fixing<br>
 """
